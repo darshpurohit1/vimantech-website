8956bb2654408dc6720dfe527a8b1cc6ba0dc99d
darshpurohit1 <darshpurohit1812@gmail.com>
2025-10-08 16:06:56 +0530

    Removed hardcoded token

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const multer = require('multer');
 const cors= require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 
